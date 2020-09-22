6d266adc64dcad326101ff57ca8ca2b4e55219e1
kalphageek <kalphageek@outlook.com>
2020-09-22 23:24:27 +0900

    fix service-start.sh

--- service-start.sh
+++ service-start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Set at jenkins executor
+appName=$1
+
+cd ~/workspace/
+DIR=$(ls | grep $appName | head -n 1)
+cd $DIR
 JAR_NAME=$(ls target/*.jar | tail -n 1)
 echo "> jar name : $JAR_NAME"
 echo "> checking pid on the running process"
@@ -12,4 +18,4 @@ else
   kill -9 $CURRENT_PID sleep 10
 fi
 echo "> deploy new version."
-java -jar $JAR_NAME
+nohup java -jar $JAR_NAME &
